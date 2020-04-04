a41e93663ddd71f6a0327c30ea60b7d8747bb913
Luane Aquino <luanecaroline91@gmail.com>
2020-04-03 22:33:37 -0300

    Updates

---
+++ precache-manifest.0e405db4a7668259ee9cf4bd608a0c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a490ab3452a8a3975969e53edf106ec5",
+    "url": "/women-can-code-react-table/index.html"
+  },
+  {
+    "revision": "6c70261b8147aa0703ba",
+    "url": "/women-can-code-react-table/static/css/main.0d82a3c1.chunk.css"
+  },
+  {
+    "revision": "9948238fb3f401656dcb",
+    "url": "/women-can-code-react-table/static/js/2.374165dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/women-can-code-react-table/static/js/2.374165dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c70261b8147aa0703ba",
+    "url": "/women-can-code-react-table/static/js/main.1d16e173.chunk.js"
+  },
+  {
+    "revision": "efbabedc1c45eddea315",
+    "url": "/women-can-code-react-table/static/js/runtime-main.0fc08703.js"
+  }
+]);
